4056f17f3ab4c082e13889fe588bda5ef53bca69
Leo - Home <18U-LMahoney@CambridgeAST.org.uk>
2022-02-08 23:59:08 +0000

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+namespace Client_Tester
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Socket sck = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+
+            IPEndPoint endPoint = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8);
+            sck.Connect(endPoint);
+
+            string msg = "lmao connected";
+            byte[] msgBuffer = Encoding.Default.GetBytes(msg);
+            sck.Send(msgBuffer, 0, msgBuffer.Length, 0);
+
+            byte[] buffer = new byte[255];
+            int rec = sck.Receive(buffer, 0, buffer.Length, 0);
+
+            Array.Resize(ref buffer, rec);
+
+            Console.WriteLine("recieved " + Encoding.Default.GetString(buffer));
+            Console.Read();
+        }
+    }
+}
